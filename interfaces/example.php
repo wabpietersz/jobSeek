5476ee81ee5468c7d57b2d244c0e382ecbe89b9d
wabpietersz <warrenpietersz@gmail.com>
2017-09-20 17:23:19 +0530

    initial

---
+++ interfaces/example.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>homepage</title>
+	<link rel = "stylesheet" type = "text/css" href = "../css/styling.css" />
+</head>
+<body >
+
+<center>
+<div class="navbar">
+	<ul>
+		<img id="image" src="../images/logo.png" style="width: 225px;height: 60px; float: right;">
+		<li><a href="" >Home</a></li>
+		<li><a href="" >Vacancies</a></li>
+		<li><a href="" >Upload CV</a></li>
+		<li><a href="" >Contact Us</a></li>
+		<li><a href="" >Get Job Alerts</a></li>
+		
+
+	</ul>
+	</div>
+</center>
+<div id="container">
+
+		<div id="adspace1">ADVERTISE HERE!</div>
+
+
+		<div id="content">
+		<form action="">
+			<div class="styled-select blue semi-square in">
+			  <select value="Select your region">
+			  	<option>Any Region</option>
+			    <option>Colombo</option>
+			    <option>Kandy</option>
+			    <option>Galle</option>
+			  </select>
+			</div>
+
+			<div class="styled-select blue semi-square in">
+			  <select value="Select your category">
+			  	<option>Any Category</option>
+			    <option>IT</option>
+			    <option>Accountant</option>
+			    <option>Clerk</option>
+			  </select>
+			</div>
+<!--searchbutton-->
+			</form>
+			<div id="vertical">
+			<button class="button" style="vertical-align:middle"><span>Find my location </span></button>
+			<nav>
+	<div class="item">
+		<input type="checkbox" id="check1">
+		<label for="check1">Select Industry</label>
+		<ul>
+			<li><a href="">Banking</a></li>
+			<li><a href="">IT</a></li>
+			<li><a href="">Teaching</a></li>
+
+		</ul>
+	</div>
+	<div class="item">
+		<input type="checkbox" id="check2">
+		<label for="check2">Location</label>
+		<ul>
+			<li><a href="">Colombo</a></li>
+			<li><a href="">Kandy</a></li>
+			<li><a href="">Galle</a></li>
+
+		</ul>
+	</div>
+	<div class="item">
+		<input type="checkbox" id="check3">
+		<label for="check3">Salary Range</label>
+		<ul>
+			<li><a href="">10000-25000</a></li>
+			<li><a href="">25000-more</a></li>
+
+		</ul>
+	</div>
+	<div class="item">
+		<input type="checkbox" id="check4">
+		<label for="check4">Job Type</label>
+		<ul>
+			<li><a href="">Full Time</a></li>
+			<li><a href="">Part Time</a></li>
+
+		</ul>
+	</div>
+
+
+</nav>
+
+
+				
+
+
+
+
+
+			</div>
+			
+		</div>
+		<div id="adspace2">ADVERTISE HERE!</div>
+		<div id="clear"></div>
+</div>
+</body>
+</html>
---
+++ interfaces/homepage.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> homepage </title>
+  	<link rel = "stylesheet" type = "text/css" href = "../css/homepage.css" />
+</head>
+<body>
+
+	<div id="vertical">
+<nav>
+	<div class="item">
+		<input type="checkbox" id="check1">
+		<label for="check1">Menu</label>
+		<ul>
+			<li><a href="">Home</a></li>
+			<li><a href="">Other</a></li>
+
+		</ul>
+	</div>
+	<div class="item">
+		<input type="checkbox" id="check2">
+		<label for="check2">Something</label>
+		<ul>
+			<li><a href="">Home</a></li>
+			<li><a href="">Other</a></li>
+
+		</ul>
+	</div>
+	<div class="item">
+		<input type="checkbox" id="check3">
+		<label for="check3">Else</label>
+		<ul>
+			<li><a href="">Home</a></li>
+			<li><a href="">Other</a></li>
+
+		</ul>
+	</div>
+	
+
+
+</nav>
+</div>
+
+
+	
+
+
+
+</body>
+
+
+
+</html>
